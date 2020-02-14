a42eb954dddc73a2961107e95803cb8f975f36ef
hrishibhattu <hrishibhattu123@gmail.com>
2020-02-14 16:09:56 +0530

    First commit

---
+++ iOS/TrashCam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrashCam
+//
+//  Created by Hrishikesh Bhattu on 14/02/20.
+//  Copyright © 2020 Hrishikesh Bhattu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
